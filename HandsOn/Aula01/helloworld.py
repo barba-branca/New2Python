0807bacfdf92dd93a3c5010f1041afb427d83ce0
Yannick Belot <yannickbh@gmail.com>
2016-08-01 20:15:20 -0300

    Final HelloWorld.py

--- HandsOn/Aula01/helloworld.py
+++ HandsOn/Aula01/helloworld.py
@@ -1,1 +1,7 @@
 print "Hello World!"
+
+#git config --global user.name "Yannick Belot"
+#git config --global user.email "yannickbh@gmail.com"
+#git add -all
+#git commit -m "mensagem commit"
+#git push
