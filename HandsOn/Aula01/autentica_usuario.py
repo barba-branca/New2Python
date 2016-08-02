01afa2d468159a52ecd5e4193fb4d375aa79c6dc
Yannick Belot <yannickbh@gmail.com>
2016-08-02 18:43:44 -0300

    ajustes para dicionario

--- HandsOn/Aula01/autentica_usuario.py
+++ HandsOn/Aula01/autentica_usuario.py
@@ -4,7 +4,11 @@ usuario = raw_input("Informe o seu usuario: " )
 
 senha = raw_input("Informe a sua senha: ")
 
-if (usuario == "Yannick") and (senha == "123456"):
+usuarios = {"Yannick":"123456"}
+print usuarios.keys()[0]
+print usuarios.get(usuarios.keys()[0])
+
+if (usuario == usuarios.keys()[0]) and (senha == usuarios.get(usuarios.keys()[0])):
     print("Usuario autenticado com sucesso")
     print("Seja Bem Vindo " +usuario)
 else:
