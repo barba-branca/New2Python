4fdd7d3b39a0bd71c6c150f6da55a75434fed440
Yannick Belot <yannickbh@gmail.com>
2016-08-03 18:46:48 -0300

    exercicio de casa

--- HandsOn/Aula02/autentica_usuario.py
+++ HandsOn/Aula02/autentica_usuario.py
@@ -1,13 +1,57 @@
 #!/usr/bin/python
 
-usuario = raw_input("Informe o seu usuario: " ) 
+usuarios = []
+senhas = []
 
-senha = raw_input("Informe a sua senha: ")
+def listar_opcoes():
+    print "1 - Cadastrar Usuario"
+    print "2 - Acessar Sistema"
+    print "3 - Sair do Sistema"
 
-usuarios = {"Yannick":"123456"}
+def switch(x):
+    dic_funcoes = {1: cadastrar_usuario,
+                   2: acessar_sistema,
+                   3: sair_sistema}
+    dic_funcoes[x]()
 
-if (usuario == usuarios.keys()[0]) and (senha == usuarios.get(usuarios.keys()[0])):
-    print("Usuario autenticado com sucesso")
-    print("Seja Bem Vindo " +usuario)
-else:
-    print("Acesso Negado")
+def sair_sistema():
+    print "Sair do Sistema"
+    exit()
+
+def cadastrar_usuario():
+    print "Cadastrar Usuario"
+    global usuarios
+    global senhas
+
+    login = raw_input("Digite o login do usuario: ")
+    senha = raw_input("Digite a senha do usuario: ")
+    usuarios.append(login)
+    senhas.append(senha)
+
+def acessar_sistema():
+
+    global usuarios
+    global senhas
+
+    usuario = raw_input("Informe o seu usuario: " ) 
+    senha = raw_input("Informe a sua senha: ")
+ 
+    for usuario in usuarios:
+
+        indice = usuarios.index(usuario)
+        
+        if usuarios.index(usuario) == 0: 
+            print "Usuario nao encontrado."
+            break
+        elif senhas[indice] != senha:
+            print "Senha nao confere"
+            break
+        else:
+            print "Acesso liberado ao Sistema"
+            print("Seja Bem Vindo " +usuario) 
+
+while True:
+    listar_opcoes()
+    opcao = int(raw_input("Digite a opcao desejada: "))
+    switch(opcao)
+    
